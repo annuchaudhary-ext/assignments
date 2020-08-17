4b8b6c59efe4418353b02fb19133e6efac12a782
annu-chaudhary <chaudharyannu7@gmail.com>
2020-08-17 12:02:07 +0530

    protractor test cases update

--- conf/conf.js
+++ conf/conf.js
@@ -4,7 +4,11 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      // --allow-file-access-from-files - allow XHR from file://
+      args: ['allow-file-access-from-files']
+  }
   },
 
   // Framework to use. Jasmine is recommended.
@@ -12,10 +16,22 @@ exports.config = {
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
-  specs: ['../test/login_spec.js'],
+  specs: ['../tests/login_spec.js'],
 
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+  // baseUrl: 'file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor',
+
+  onPrepare: function() {
+    // By default, Protractor use data:text/html,<html></html> as resetUrl, but 
+    // location.replace from the data: to the file: protocol is not allowed
+    // (we'll get ‘not allowed local resource’ error), so we replace resetUrl with one
+    // with the file: protocol (this particular one will open system's root folder)
+    browser.ignoreSynchronization = true;
+    browser.waitForAngular();
+    browser.sleep(500); 
+    browser.resetUrl = 'file:///';
   }
 };
---
+++ tests/login_spec.js
@@ -0,0 +1,86 @@
+describe("Login App Test", function() {
+    it("When both email and password invalid credentials passed", function() {
+        browser.get("file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor/src/index.html");
+        var emailInp = element(by.name("email"));
+        var passInp = element(by.name("password"));
+        var submitBtn = element(by.id("loginSubmitBtn"));
+
+        emailInp.sendKeys("chaudharyannugmail.com");
+        passInp.sendKeys("annu");
+
+        submitBtn.click();
+
+        expect(element(by.id("emailError")).getText()).toEqual("You have entered invalid email address");
+        expect(element(by.id("passwordError")).getText()).toEqual("You have entered invalid password");
+        browser.sleep(2000);
+        
+    });
+
+    it("When invalid email credential passed", function() {
+        browser.get("file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor/src/index.html");
+        var emailInp = element(by.name("email"));
+        var passInp = element(by.name("password"));
+        var submitBtn = element(by.id("loginSubmitBtn"));
+
+        emailInp.sendKeys("chaudharyannugmail.com");
+        passInp.sendKeys("Annu@1212");
+
+        submitBtn.click();
+
+        expect(element(by.id("emailError")).getText()).toEqual("You have entered invalid email address");
+        expect(element(by.id("passwordError")).actionResults_).toBeUndefined();
+        browser.sleep(2000);
+        
+    });
+
+    it("When invalid password credential passed", function() {
+        browser.get("file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor/src/index.html");
+        var emailInp = element(by.name("email"));
+        var passInp = element(by.name("password"));
+        var submitBtn = element(by.id("loginSubmitBtn"));
+
+        emailInp.sendKeys("chaudharyannu7@gmail.com");
+        passInp.sendKeys("annu");
+
+        submitBtn.click();
+
+        expect(element(by.id("emailError")).actionResults_).toBeUndefined();
+        expect(element(by.id("passwordError")).getText()).toEqual("You have entered invalid password");
+        browser.sleep(2000);
+        
+    });
+
+    it("When incorrect email password passed", function() {
+        browser.get("file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor/src/index.html");
+        var emailInp = element(by.name("email"));
+        var passInp = element(by.name("password"));
+        var submitBtn = element(by.id("loginSubmitBtn"));
+
+        emailInp.sendKeys("chaudharyannu@gmail.com");
+        passInp.sendKeys("Annu@1212");
+
+        submitBtn.click();
+
+        expect(element(by.id("emailError")).actionResults_).toBeUndefined();
+        expect(element(by.id("passwordError")).actionResults_).toBeUndefined();
+        expect(element(by.id("incorrectCredError")).getText()).toEqual("Username and password entered by you is/are incorrect.");
+        browser.sleep(2000);
+        
+    });
+
+    it("When correct email password passed", function() {
+        browser.get("file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor/src/index.html");
+        var emailInp = element(by.name("email"));
+        var passInp = element(by.name("password"));
+        var submitBtn = element(by.id("loginSubmitBtn"));
+
+        emailInp.sendKeys("chaudharyannu7@gmail.com");
+        passInp.sendKeys("Annu@1234");
+
+        submitBtn.click();
+
+        expect(browser.getCurrentUrl()).toBe("file:///D://Nag%20Assignments/Login%20App%20test%20using%20Protractor/src/home.html");
+        browser.sleep(2000);
+        
+    });
+})
